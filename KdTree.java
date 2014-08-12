faf21d6690b32bd4d89d9a3bfde24e82874b217b
dmsehuang <dmsehuang@yahoo.com>
2014-08-12 12:26:10 -0700

    finish KdTree code, ready to test

--- KdTree.java
+++ KdTree.java
@@ -59,6 +59,15 @@ public class KdTree {
         }
         return node;
     }
+
+    // add the point p to the set (if it is not already in the set)    
+    public void insert(Point2D p) {
+        if (!contains(p)) {
+            RectHV rect = new RectHV(0, 0, 1, 1);
+            root = put(root, rect, p, true);
+            set.add(p);
+        }
+    }
     
     // helper method to search the node
     // compX is the parameter means to compare items horizontally
@@ -81,32 +90,74 @@ public class KdTree {
         }
     }
 
-    // add the point p to the set (if it is not already in the set)    
-    public void insert(Point2D p) {
-        if (!contains(p)) {
-            RectHV rect = new RectHV(0, 0, 1, 1);
-            root = put(root, rect, p, true);
-            set.add(p);
-        }
-    }
-
     // does the set contain the point p?
     public boolean contains(Point2D p) {
         return search(root, p, true);
     }
+    
+    // helper method to draw Kdtree, if hori is true, draw horizontal line
+    public void drawNode(Node node, boolean hori) {
+        if (node == null) return ;
+        // if node is not null, draw the current node
+        RectHV rect = node.rect;
+        if (hori) {
+            StdDraw.setPenColor(StdDraw.BLUE);
+            StdDraw.setPenRadius(.01);
+            Point2D l = new Point2D(rect.xmin(), node.p.y());
+            Point2D r = new Point2D(rect.xmax(), node.p.y());
+            l.drawTo(node.p);
+            r.drawTo(node.p);
+        }else {
+            StdDraw.setPenColor(StdDraw.RED);
+            StdDraw.setPenRadius(.01);
+            Point2D b = new Point2D(node.p.x(), rect.ymin());
+            Point2D t = new Point2D(node.p.x(), rect.ymax());
+            b.drawTo(node.p);
+            t.drawTo(node.p);
+        }
+        // draw the left and the right child, vertically
+        drawNode(node.lb, !hori);
+        drawNode(node.rt, !hori);
+    }
 
     // draw all of the points to standard draw
     public void draw() {
-
+        drawNode(root, false);
     }
     
     // all points in the set that are inside the rectangle
     public Iterable<Point2D> range(RectHV rect) {
-        
+        Stack<Point2D> ret = new Stack<Point2D>(); // store the points
+        Stack<Node> stk = new Stack<Node>(); // store the unchecked node
+        stk.push(root);
+        while (!stk.isEmpty()) {
+            Node node = stk.pop(); 
+            if (rect.contains(node.p)) ret.push(node.p); // push the 2d point in the result
+            if (rect.intersects(node.lb.rect)) stk.push(node.lb); // push left child to the stack
+            if (rect.intersects(node.rt.rect)) stk.push(node.rt); // push right child to the stack  
+        }
+        return ret;
     }
     
     // a nearest neighbor in the set to p; null if set is empty
     public Point2D nearest(Point2D p) {
-
+        if (set.size() == 0) return null;
+        Point2D point = null;
+        double min = Double.MAX_VALUE;
+        Stack<Node> stk = new Stack<Node>(); // store the unchecked node
+        stk.push(root);
+        while (!stk.isEmpty()) {
+            Node node = stk.pop();
+            double dist = node.p.distanceSquaredTo(p);
+            if (dist < min) {
+                min = dist;
+                point = node.p;
+            }
+            double left = node.lb.rect.distanceSquaredTo(p);
+            double right = node.rt.rect.distanceSquaredTo(p);
+            if (left < min) stk.push(node.lb);
+            if (right < min) stk.push(node.rt);
+        }
+        return point;
     }  
 }
